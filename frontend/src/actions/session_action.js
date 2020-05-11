e6605f552b7bb6bf3fe5040afcd91fb0ffcf84b7
breakfasting <breakfast.ing@gmail.com>
2020-05-11 12:34:39 -0700

    implement redux cycle for user auth

---
+++ frontend/src/actions/session_action.js
@@ -0,0 +1,61 @@
+import jwtDecode from 'jwt-decode';
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+// export const RECEIVE_USER_SIGN_IN = 'RECEIVE_USER_SIGN_IN';
+
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+// const receiveUserSignIn = () => ({
+//   type: RECEIVE_USER_SIGN_IN
+// });
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const login = (user) => (dispatch) => APIUtil.login(user)
+  .then((res) => {
+    const { token } = res.data;
+
+    localStorage.setItem('jwtToken', token);
+    SessionAPIUtil.setAuthToken(token)
+      .then(() => {
+        const decoded = jwtDecode(token);
+        dispatch(receiveCurrentUser(decoded));
+      });
+  })
+  .catch((err) => {
+    dispatch(receiveErrors(err.response.data));
+  });
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('jwtToken');
+  return SessionAPIUtil.setAuthToken(false)
+    .then(() => dispatch(logoutCurrentUser()));
+}
+
+export const signup = (user) => (dispatch) => SessionAPIUtil.signup(user)
+  .then((res) => {
+    const { token } = res.data;
+
+    localStorage.setItem('jwtToken', token);
+    SessionAPIUtil.setAuthToken(token)
+      .then(() => {
+        const decoded = jwtDecode(token);
+        dispatch(receiveCurrentUser(decoded));
+      });
+  })
+  .catch((err) => {
+    dispatch(receiveErrors(err.response.data));
+  });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import jwtDecode from 'jwt-decode';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { setAuthToken } from './util/session_api_util';
+import configureStore from './store/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwtDecode(localStorage.jwtToken);
+    const preloadedState = {
+      session: {
+        isAuthenticated: true,
+        user: decodedUser,
+      }
+    };
+
+    store = configureStore(preloadedState);
+    const currentTime = Date.now() / 1000;
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  } else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root)
+})
