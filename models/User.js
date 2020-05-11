e10e6f4bf9beb5472d28e7ad12a43ca8b7d8757d
steventouba <steventouba1@gmail.com>
2020-05-11 10:18:59 -0700

    Add Sample User Schema

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const UserSchema = new Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
