0a9f0d70ac093e758932cd920abb97a174a8f570
breakfasting <breakfast.ing@gmail.com>
2020-05-12 15:08:20 -0700

    add eslint/css-loader/react-router-dom

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from 'react-router-dom';
 // import LoginContainer from './users/login_container.js';
 // import SignupContainer from './users/signup_container';
 // import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 // import MainPage from "./main/main_page"
-import Grid from "./game/grid"
-
-
+import Grid from './game/grid';
+import UI from './ui/ui';
 
 function App() {
   return (
     <div className="App">
-    <Switch>
+      <Switch>
         {/* <AuthRoute exact path="/login" component={LoginContainer} />
         <AuthRoute exact path="/signup" component={SignupContainer} />
         <AuthRoute exact path="/" component={MainPage} /> */}
-        <Route path="/" component={Grid}/>
-    </Switch>
-   
-    
-
+        <Route path="/" component={Grid} />
+        <Route path="/" component={UI} />
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/components/ui/ui.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UI extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        
+      </div>
+    );
+  }
+}
+
+export default UI;
