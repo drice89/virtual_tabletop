f88c30530df694cba9d1db25870cc61d8db589cd
steventouba <steventouba1@gmail.com>
2020-05-11 18:48:19 -0700

    Add game schema

---
+++ models/Game.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+const GameSchema = new Schema({
+  creatorId: {
+    type: Schema.Types.ObjectId, 
+    ref: 'users'
+  }, 
+  name: { 
+    type: String, 
+    required: true, 
+  }, 
+  description: { 
+    type: String, 
+  }, 
+  backgroundImage: {
+    type: String
+  }, 
+  players: [{ 
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }], 
+  boards: [{ 
+    type: Schema.Types.ObjectId,
+    ref: 'boards'
+  }], 
+})
+
+module.exports = Game = mongoose.model('Game', GameSchema); 
