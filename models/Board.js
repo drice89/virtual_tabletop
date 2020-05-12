b48fe9ac8e77f64840dcc816aff9eb2fabf3dd57
drice89 <drice89@gmail.com>
2020-05-12 11:40:36 -0700

    Add board and piece schema

---
+++ models/Board.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+const BoardSchema = new Schema({
+  gameId: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  gridSize: { 
+    rows: {
+      type: Number,
+      required: true
+    },
+    cols: {
+      type: Number,
+      required: true
+    }
+  },
+  backgroundImageUrl: {
+    type: String,
+    required: true
+  },
+  squareSize: {
+    offsetX: {
+      type: Number,
+      required: true
+    },
+    offsetY: {
+      type: Number,
+      required: true
+    },
+    zoomFactor: {
+      type: Number,
+      required: true
+    }
+  },
+  settings: {
+    gridColor: {
+      type: String,
+      default: '#808080'
+    },
+    opacity: {
+      type: { 
+        min: 0, 
+        max: 1
+      },
+      default: 1
+    }
+  },
+  tokens: [
+    {
+      _id: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      pos: {
+        x : {
+          type: Number,
+          required: true
+        },
+        y: {
+          type: Number,
+          required: true
+        }
+      },
+      size: {
+        type: Number,
+        default: 1,
+      },
+      pieceId: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      player: {
+        type: Schema.Types.ObjectId
+      }
+    }
+  ]
+});
+
+module.exports = Board = mongoose.model("Board", BoardSchema);
