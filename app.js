fed9af6d6de4d456dc674384d4457482470bb87b
Dias Iskrayev <sparkymusic22@gmail.com>
2020-05-13 10:43:22 -0700

    heroku test

--- app.js
+++ app.js
@@ -25,12 +25,14 @@ app.use('/api/users', users);
 app.use('/api/games', games); 
 app.use('/api/boards', boards); 
 
+console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
+
 }
 
 const port = process.env.PORT || 5000;
