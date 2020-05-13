b0d88724acafb3707ff5dd1cea0e76b876eca538
breakfasting <breakfast.ing@gmail.com>
2020-05-12 20:06:03 -0700

    ui left nav mockup

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -7,7 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 import Grid from './game/grid';
 import UI from './ui/ui';
 import './reset.css';
-import './structure.css';
+import './structure.scss';
 
 function App() {
   return (
