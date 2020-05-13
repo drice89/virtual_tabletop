36d9a87bd9c3148bde58e3a01ed7d6911bb57187
drice89 <drice89@gmail.com>
2020-05-13 00:05:30 -0700

    Add prod state

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use('/api/users', users);
 app.use('/api/games', games); 
 app.use('/api/boards', boards); 
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
