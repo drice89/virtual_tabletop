595a1687955b5641f9882fb0841668295244c6ec
breakfasting <breakfast.ing@gmail.com>
2020-05-14 13:32:54 -0700

    create new game mockup

---
+++ frontend/src/components/user/create_game.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './create_game.module.scss';
+import logoStyles from '../logo/logo.module.scss';
+import buttons from '../buttons.module.scss';
+
+class CreateGame extends React.Component {
+  constructor(props) {
+    super(props);
+    const { creatorId } = 'creatorId';
+    this.state = {
+      name: '',
+      description: '',
+      backgroundImage: '',
+      creatorId,
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { login } = this.props;
+    login(this.state);
+  }
+
+  handleChange(form) {
+    return (e) => {
+      this.setState({ [form]: e.target.value });
+    };
+  }
+
+  render() {
+    const { name, description, backgroundImage } = this.state;
+    return (
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <i className="ra ra-anvil" />
+          <p>Create New Game</p>
+        </div>
+        <form className={styles.formContainer} onSubmit={this.handleSubmit}>
+          {/* {errors.name ? <span className={styles.errors}>{errors.name}</span> : ''} */}
+          <input type="text" placeholder="Name" value={name} onChange={this.handleChange('name')} />
+          {/* {errors.description ? <span className={styles.errors}>{errors.description}</span> : ''} */}
+          <input type="text" placeholder="Description" value={description} onChange={this.handleChange('description')} />
+          {/* {errors.backgroundImage ? <span className={styles.errors}>{errors.backgroundImage}</span> : ''} */}
+          <input type="text" placeholder="Thumbnail URL" value={backgroundImage} onChange={this.handleChange('backgroundImage')} />
+          <button type="submit" className={buttons.secondary}>Create</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateGame;
