0fc1933c84fd6fe2b0b2fa46a64ca27bcc4ad940
breakfasting <breakfast.ing@gmail.com>
2020-05-14 00:34:33 -0700

    scroll background that fits

--- frontend/src/components/user/user_show.jsx
+++ frontend/src/components/user/user_show.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './user_show.module.scss';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class UserShow extends React.Component {
   render() {
     return (
-      <div>
-        Hi this is user show
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <div className={styles.test}>lorem1000</div>
+        </div>
       </div>
     );
   }
