25450cc3cb5aa9404df04481beb8cf3cdc3cc704
Dillon Rice <drice89@gmail.com>
2020-05-14 11:09:50 -0700

    Add board and token actions

---
+++ frontend/src/util/tokens_api_util.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const createToken = (board) => (
+  axios.post('')
+);
+const editToken = (board) => (
+  axios.patch('')
+);
+const deleteToken = (board) => (
+  axios.delete('')
+);
