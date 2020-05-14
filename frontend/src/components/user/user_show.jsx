8aa78f1f06691bf4c1f92983fb3387e4234973bb
breakfasting <breakfast.ing@gmail.com>
2020-05-14 02:13:53 -0700

    user show page mockup

--- frontend/src/components/user/user_show.jsx
+++ frontend/src/components/user/user_show.jsx
@@ -7,7 +7,35 @@ class UserShow extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.background}>
-          <div className={styles.test}>lorem1000</div>
+          <div className={styles.contentContainer}>
+            <div className={styles.profile}>
+              <img className={styles.profileImg} src="https://via.placeholder.com/150/000000" alt="" />
+              <div>
+                <h1>Username</h1>
+                <span>Level 7 Novice</span>
+              </div>
+              <div className={styles.stats}>
+                <div>
+                  <h1>2</h1>
+                  Created Games
+                </div>
+                <div>
+                  <h1>5</h1>
+                  Joined Games
+                </div>
+              </div>
+            </div>
+            <div className={styles.content}>
+              <section className={styles.main}>
+                <h2>Created Games</h2>
+              </section>
+              <aside className={styles.sidebar}>
+                <ul>
+                  <li>friend-1</li>
+                </ul>
+              </aside>
+            </div>
+          </div>
         </div>
       </div>
     );
