be81c3b0698465209a52664cf15b58b9fc9130b4
breakfasting <breakfast.ing@gmail.com>
2020-05-14 11:09:47 -0700

    finish user show page mockout

--- frontend/src/components/user/user_show.jsx
+++ frontend/src/components/user/user_show.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import map from '../../images/battlemap.jpg';
+import snow from '../../images/snow_landscape.jpg';
 import styles from './user_show.module.scss';
+import buttons from '../buttons.module.scss';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class UserShow extends React.Component {
@@ -26,14 +29,85 @@ class UserShow extends React.Component {
               </div>
             </div>
             <div className={styles.content}>
+              <div className={styles.topBar}>
+                <h2>My Games</h2>
+                <div>
+                  <button type="button" className={`${buttons.secondary} ${buttons.btnIcon}`}>
+                    <i className="ra ra-anvil ra-lg" />
+                    <span>
+                      Create New Game
+                    </span>
+                  </button>
+                </div>
+              </div>
               <section className={styles.main}>
-                <h2>Created Games</h2>
+                <div className={styles.gameContainer}>
+                  <div className={styles.card}>
+                    <div className={styles.status}>
+                      <div className={styles.dot}>⬤</div>
+                      <span>4 Online</span>
+                    </div>
+                    <div className={styles.title}>
+                      <h1>Wayne's World</h1>
+                      <span>The example description of Wayne's World</span>
+                    </div>
+                    <div className={styles.cardBack} style={{ backgroundImage: `url(${snow})` }} />
+                  </div>
+                </div>
+                {/* <img src={map} alt=""/> */}
+                <div className={styles.gameContainer}>
+                  <div className={styles.card} style={{}}>
+                    <div className={styles.status}>
+                      <div className={styles.dot} style={{ color: '#ccc' }}>⬤</div>
+                      <span>Offline</span>
+                    </div>
+                    <div className={styles.title}>
+                      <h1>Dillon's World</h1>
+                      <span>The example description of Dillon's World</span>
+                    </div>
+                    <div className={styles.cardBack} style={{ backgroundImage: `url(${map})` }} />
+                  </div>
+                </div>
+                <div className={styles.gameContainer}>
+                  <div className={styles.card} style={{}}>
+                    <div className={styles.status}>
+                      <div className={styles.dot} style={{ color: '#ccc' }}>⬤</div>
+                      <span>Offline</span>
+                    </div>
+                    <div className={styles.title}>
+                      <h1>Dillon's World</h1>
+                      <span>The example description of Dillon's World</span>
+                    </div>
+                    <div className={styles.cardBack} style={{ backgroundImage: `url(${map})` }} />
+                  </div>
+                </div>
+                <div className={styles.gameContainer}>
+                  <div className={styles.card} style={{}}>
+                    <div className={styles.status}>
+                      <div className={styles.dot} style={{ color: '#ccc' }}>⬤</div>
+                      <span>Offline</span>
+                    </div>
+                    <div className={styles.title}>
+                      <h1>Dillon's World</h1>
+                      <span>The example description of Dillon's World</span>
+                    </div>
+                    <div className={styles.cardBack} style={{ backgroundImage: `url(${map})` }} />
+                  </div>
+                </div>
+                <div className={styles.gameContainer}>
+                  <div className={styles.card} style={{}}>
+                    <div className={styles.status}>
+                      <div className={styles.dot} style={{ color: '#ccc' }}>⬤</div>
+                      <span>Offline</span>
+                    </div>
+                    <div className={styles.title}>
+                      <h1>Dillon's World</h1>
+                      <span>The example description of Dillon's World</span>
+                    </div>
+                    <div className={styles.cardBack} style={{ backgroundImage: `url(${map})` }} />
+                  </div>
+                </div>
               </section>
-              <aside className={styles.sidebar}>
-                <ul>
-                  <li>friend-1</li>
-                </ul>
-              </aside>
             </div>
           </div>
         </div>
