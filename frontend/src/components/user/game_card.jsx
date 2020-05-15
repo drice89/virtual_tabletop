16efa4d7ee5e1cd815fc111b343659c15f15bd28
breakfasting <breakfast.ing@gmail.com>
2020-05-14 19:18:30 -0700

    render all values for usershow

--- frontend/src/components/user/game_card.jsx
+++ frontend/src/components/user/game_card.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './game_card.module.scss';
 import Status from './status';
+import noThumb from '../../images/noThumb.webp';
 
 const GameCard = ({
   game: {
-    title, description, thumb, online,
+    name, description, backgroundImage, online = 0,
   },
 }) => (
   <div className={styles.gameContainer}>
     <div className={styles.card}>
       <Status online={online} />
       <div className={styles.title}>
-        <h1>{title}</h1>
+        <h1>{name}</h1>
         <span>{description}</span>
       </div>
-      <div className={styles.cardBack} style={{ backgroundImage: `url(${thumb})` }} />
+      <div className={styles.cardBack} style={{ backgroundImage: `url(${backgroundImage || noThumb})` }} />
     </div>
   </div>
 );
--- frontend/src/components/user/user_show_container.js
+++ frontend/src/components/user/user_show_container.js
@@ -2,10 +2,16 @@ import { connect } from 'react-redux';
 import UserShow from './user_show';
 import { fetchUser } from '../../actions/users_actions';
 
-const mapStateToProps = (state, ownProps) => ({
-  user: state.entities.users[ownProps.match.params.userId],
-  errors: state.errors.users,
-});
+const mapStateToProps = (state, ownProps) => {
+  const user = state.entities.users[ownProps.match.params.userId];
+  // debugger;
+  return {
+    user,
+    createdGames: user.createdGames ? user.createdGames.map((game) => state.entities.games[game]) : [],
+    subscribedGames: user.subscribedGames ? user.subscribedGames.map((game) => state.entities.games[game]) : [],
+    errors: state.errors.users,
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchUser: () => dispatch(fetchUser(ownProps.match.params.userId)),
